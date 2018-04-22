be4b5abcde4e07268dc591827d124015ff3ebc51
Dennis Wang <fengw@cn.ibm.com>
2018-04-22 14:11:29 +0800

    Change to Dictory Learning

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,14 @@
-print("Hello Python World!")
-print("Hello every on!")
+favorite_languages = {
+	'jen': 'Python',
+	'dennis': 'C',
+	'sarch': 'ruby',
+	'phil': 'Python',
+	}
+
+friends = ['dennis','jen']
+
+for name in  favorite_languages.keys():
+	print(name.title())
+
+	if name in friends:
+		print("    Hi " + name.title() + ",I see your favorite language is " + favorite_languages[name]+"!")
